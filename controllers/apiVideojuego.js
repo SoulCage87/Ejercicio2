48a0edc191e4ac4cae45d35b9fde0774a2b5b1e8
Samuel Galo <samuelgalo1625@gmail.com>
2024-02-02 20:59:51 -0600

    Commit 2
    Relacion de Tablas

--- controllers/apiVideojuego.js
+++ controllers/apiVideojuego.js
@@ -2,20 +2,23 @@ import {db} from '../db/conn.js';
 
 const getJuego = async (req, res) => {
  
-    const sql = `SELECT * FROM tbl_videjuegos ORDER BY id`;
+    const sql = `SELECT a.*, b.nombre_genero AS genero, c.num_pasillo AS pasillo, c.num_estante AS estante
+    FROM tbl_videjuegos a
+    INNER JOIN tbl_genero b ON a.id_genero = b.id
+    INNER JOIN tbl_pasillo c ON a.id_pasillo = c.id;`;
     const result = await db.query(sql);
     res.json(result);
 };
 
 const postJuego = async (req,res) => {
 
-   const {nombre, desarrollador} = req.body;
-   const params = [nombre,desarrollador];
+   const {nombre, desarrollador,id_genero,id_pasillo} = req.body;
+   const params = [nombre,desarrollador,id_genero,id_pasillo];
 
    const sql = `INSERT INTO tbl_videjuegos
-                (nombre,desarrollador)
+                (nombre,desarrollador,id_genero,id_pasillo)
                 VALUES
-                ($1, $2) returning *`;
+                ($1, $2, $3, $4) returning *`;
 
    const result = await db.query(sql, params); 
    res.json(result);            
